ab2c2465cdc6f644910c7b9b3552d3ff926c8d84
MatheusCostaLara-dev <matheuscostalara@hotmail.com>
2024-02-28 14:01:29 -0300

    001

---
+++ Clinica/src/main/java/Tela/TelaPrincipal.java
@@ -0,0 +1,8 @@
+package Tela;
+
+public class TelaPrincipal  {
+   //tudo começa a partir de um main
+    public static void main(String[] args) {
+
+    }
+}
---
+++ fundamentos/src/main/java/exercicios/PrimeiroPrograma01.java
@@ -0,0 +1,10 @@
+package exercicios;
+
+public class PrimeiroPrograma01 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!!");
+        System.out.println("Primeiro Programa parte #01");
+        System.out.println("Primeiro Programa parte #02");
+    }
+}
